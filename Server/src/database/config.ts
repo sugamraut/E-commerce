61186b3fb7c798fe097de5ead1c218e91b8a9dfd
Sugam Raut <sugamrautbim@gmail.com>
2025-06-02 22:29:12 +0545

    we covered Relationship Order

--- Server/src/database/config.ts
+++ Server/src/database/config.ts
@@ -2,6 +2,10 @@ import { Sequelize } from "sequelize-typescript";
 import { envConfig } from "../config/config";
 import product from "./models/productModel";
 import category from "./models/categoryModel";
+import User from "./models/userModel";
+import Order from "./models/orderModel";
+import Payments from "./models/paymentModel";
+import OrderDetail from "./models/orderDetails";
 
 
 const sequelize=new Sequelize(envConfig.connectionString as string,{models :[__dirname + '/models']})
@@ -26,7 +30,18 @@ sequelize.sync({force : false,alter:true}).then(()=>{
 //relationships//
 product.belongsTo(category,{foreignKey:'categoryId'})
 category.hasOne(product,{foreignKey:'categoryId'})
+//user X order
+Order.belongsTo(User,{foreignKey:'userId'})
+User.hasMany(Order,{foreignKey:'userId'})
 
+//payment X order payment ma order id chaeyo
+Payments.belongsTo(Order,{foreignKey:'orderId'})
+Order.hasOne(Payments,{foreignKey:'orderId'})
+// order X orderDetails orderDetails ma order id chayo bhana
+OrderDetail.belongsTo(Order,{foreignKey:'orderId'})
+Order.hasOne(OrderDetail,{foreignKey:'orderId'})
 
+product.hasMany(OrderDetail,{foreignKey:"productId"})
+OrderDetail.belongsTo(product,{foreignKey:"productId"})
 
 export default sequelize
